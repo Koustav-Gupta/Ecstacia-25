97c9ccab347db872874da054b00d31c20c0feaf5
Koustav Gupta <106402479+Koustav-Gupta@users.noreply.github.com>
2025-02-08 21:54:23 +0530

    empty App.tsx

--- App.tsx
+++ App.tsx
@@ -1,14 +1,1 @@
-import React from "react";
-import ImageMarquee from "./ImageMarquee"; // Remove `.jsx`, TypeScript resolves `.tsx` automatically
 
-function App() {
-  return (
-    <div className="App">
-      {/* Other components or sections */}
-      <ImageMarquee />
-      {/* Other components or sections */}
-    </div>
-  );
-}
-
-export default App;
